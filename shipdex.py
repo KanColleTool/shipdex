c317a9a904b3f915a58ab32fa546555e1aa6fa2e
uppfinnarn <uppfinnarn@gmail.com>
2014-04-02 13:22:38 +0200

    Site base

--- shipdex.py
+++ shipdex.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/ships/')
+def ships():
+	return render_template('ships.html')
+
+@app.route('/ships/<name>/')
+def ship(name):
+	return render_template('ship.html', name=name)
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
