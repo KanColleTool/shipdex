7858f365b81a219dd54cd3d347091a275889b143
uppfinnarn <uppfinnarn@gmail.com>
2014-04-05 19:57:41 +0200

    Frontend base

--- shipdex.py
+++ shipdex.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from util import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	ships = load_data('cache', 'ships.json')
+	return render_template('index.html', ships=ships, breadcrumb=['Home'])
 
-@app.route('/ships/')
-def ships():
-	return render_template('ships.html')
-
-@app.route('/ships/<name>/')
+@app.route('/<name>/')
 def ship(name):
-	return render_template('ship.html', name=name)
+	ship = load_data('cache', 'ships/{name}.json'.format(name=normalize_name(name)))
+	return render_template('ship.html', ship=ship, breadcrumb=[(url_for('index'), 'Home'), ship['api_name']])
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
